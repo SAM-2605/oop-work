f4757af754dc63dcdcc2e84ec56613a1289357d2
SAM-2605 <92805880+SAM-2605@users.noreply.github.com>
2022-10-20 17:29:01 +0300

    Add files via upload
    This is my very first Assignment

---
+++ assignment 1/ask_me_2.py
@@ -0,0 +1,7 @@
+# programme that validates name if no name is entered it  displays hello stranger
+name=str(input("hello may you enter your name please: "))
+#statement to check the data entered 
+if name == "":
+    print("hello, how you doing stranger")
+else:    
+    print("hello how you doing",name)
